3fc3add4c37638577b7db097ade96623e1474728
Vo Thanh Lam <vothanhlam1793@gmail.com>
2021-12-15 00:53:50 +0700

    flash sale

--- main.js
+++ main.js
@@ -10,6 +10,23 @@ var corsOptions = {
 };
 const fileUpload = require('express-fileupload');
 
+const db = require("./app/models");
+db.mongoose
+    .connect(db.url, {
+      "user": "black",
+      "pass": "asrkpvg7",
+      authSource: "admin",
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+});
+
 app.use(fileUpload());
 app.use(logger('dev'));
 app.use(cors(corsOptions));
@@ -27,6 +44,8 @@ app.use(express.urlencoded({ extended: true }));
 require("./route/tool.route")(app);
 require("./route/customer")(app);
 app.use("/baogia", require("./route/baogia"));
+app.use("/product", require("./route/product"));
+require("./app/routes/product.route")(app);
 
 app.get('*', function(req, res){
   res.render("customer/index", {
